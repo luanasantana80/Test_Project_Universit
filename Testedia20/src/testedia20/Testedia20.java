4a23e10e50a0da8dbe979660083426e9267dc9b9
Luana Caroline <93920703+luanasantana80@users.noreply.github.com>
2023-08-04 13:53:36 -0300

    Add files via upload

---
+++ Testedia20/src/testedia20/Testedia20.java
@@ -0,0 +1,21 @@
+
+package testedia20;
+
+import javax.swing.JOptionPane;
+
+
+public class Testedia20 {
+
+   
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Sem icone","Mensagem Sem Icone",JOptionPane.PLAIN_MESSAGE );
+        JOptionPane.showMessageDialog(null,"Sem icone","Mensagem Information",JOptionPane.INFORMATION_MESSAGE );
+        JOptionPane.showMessageDialog(null,"Sem icone","Mensagem Sem Icone",JOptionPane.PLAIN_MESSAGE );
+        
+        
+        
+        
+        
+    }
+    
+}
